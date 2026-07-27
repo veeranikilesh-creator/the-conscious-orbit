57a3bc7ed33c4da544922331d999282ba660915f
Syed-afridi-7 <tutyafridi216@gmail.com>
2026-07-27 18:31:09 +0530

    Fix initialization crash on server startup and add deployment configuration

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -3,11 +3,19 @@ import { env } from './env.js';
 
 export async function connectDb() {
   mongoose.set('strictQuery', true);
-  await mongoose.connect(env.mongoUri, { serverSelectionTimeoutMS: 10_000 });
-  console.log(`[db] connected to ${mongoose.connection.name}`);
-  return mongoose.connection;
+  try {
+    await mongoose.connect(env.mongoUri, { serverSelectionTimeoutMS: 5_000 });
+    console.log(`[db] connected to ${mongoose.connection.name}`);
+    return mongoose.connection;
+  } catch (err) {
+    console.warn(`[db] warning: could not connect to MongoDB (${err.message}). Server running with DB disconnected.`);
+    return null;
+  }
 }
 
 export async function disconnectDb() {
-  await mongoose.disconnect();
+  if (mongoose.connection.readyState !== 0) {
+    await mongoose.disconnect();
+  }
 }
+
